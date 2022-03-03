72c84b5465af87309d5f3d2f99cd723fbbd2f4d2
imsern <kristofferims@hotmail.com>
2022-03-03 17:17:56 +0100

    Removed text
    Changed the text on homepage. Going to be added to landing page later on

--- pages/components/home.js
+++ pages/components/home.js
@@ -7,31 +7,7 @@ import linkedin from "../../public/images/icons/linkedin.png";
 const HomePage = () => {
 	return (
 		<>
-			<div className="mt-6 text-lg">
-				<p>
-					We don´t want to be your average agency. We want to be part of your
-					team and work solely for your success. We work exclusively in the
-					fitness niche. Regardless, we focus on what will and is generating you
-					revenue.
-				</p>
-
-				<p className="mt-4">
-					We understand how your business works inside and out, and with this we
-					can help your business to get what it really needs, instead of
-					guessing.
-				</p>
-
-				<p className="mt-3 font-bold">
-					You know how to change someones life into what they want. <br />
-					We know how to reach them for you. <br />
-					Lets build an amazing team together. <br />
-				</p>
-
-				<p className="mt-4 mb-20">
-					We help the fitness industry with reaching its audience with FB/IG
-					ads.
-				</p>
-			</div>
+			
 			<div className="flex gap-2 flex-wrap w-2/4 m-auto md:flex-nowrap md:w-fit mb-10">
 				<div className="flex-grow mt-10 md:mt-0">
 					<h1 className="text-xl font-bold">Patrick Nordhagen</h1>
